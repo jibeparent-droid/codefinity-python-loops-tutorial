1e8c8783dae2455f3e9d11092a18ad102e346589
jibeparent-droid <>
2026-01-01 13:17:03 +0000

    Enumerate Usage

---
+++ the_for_loop/enumerate_usage/main.py
@@ -0,0 +1,12 @@
+# Full list of countries you are considering for travel
+countries = ['Wales', 'Denmark', 'Belgium', 'South Korea', 'Barcelona', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Tokyo', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Paris', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Liverpool', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'New York', 'China', 'Munchen', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# Initialize an empty list for rankings
+travel_rankings = []
+
+# Use enumerate to label each destination
+for index, country in enumerate(countries):
+    travel_rankings.append(f"Destination {index+1}: {country}")
+
+# Testing
+print("Travel destination rankings:", travel_rankings)
