8256ee2992dc5cb83b4088074fb38e21bf8bd8c1
jibeparent-droid <>
2026-01-01 11:51:30 +0000

    For loops

---
+++ the_for_loop/for_loops/main.py
@@ -0,0 +1,9 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'Barcelona', 'South Africa', 'Indonesia','Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Tokyo', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Paris', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Liverpool', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'New York', 'China', 'Munchen', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of visited countires
+travel_list = []
+for countrie in countries:
+    travel_list.append(countrie)
+# Testing
+print("Countries to visit:", travel_list)
